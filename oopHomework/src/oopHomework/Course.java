f4abbf186fc2db0762eb3f4da0254115b612f7b9
Gürkan Sönmez <56911478+Snmzgrkn@users.noreply.github.com>
2021-04-26 13:32:56 +0300

    Homework 1

---
+++ oopHomework/src/oopHomework/Course.java
@@ -0,0 +1,18 @@
+package oopHomework;
+
+public class Course {
+	
+	int id;
+	String name;
+	String lecturer;
+	String price;
+
+	public Course(int id,String name,String lecturer,String price) {
+		this.id = id;
+		this.name = name;
+		this.lecturer = lecturer;
+		this.price = price;
+		
+	}
+
+}
