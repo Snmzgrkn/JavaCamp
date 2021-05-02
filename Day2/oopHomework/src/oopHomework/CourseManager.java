b5dd38e0bfb47641a9b82500b9c3be9e03d26566
Gürkan Sönmez <56911478+Snmzgrkn@users.noreply.github.com>
2021-05-02 13:53:07 +0300

    Day2

---
+++ Day2/oopHomework/src/oopHomework/CourseManager.java
@@ -0,0 +1,13 @@
+package oopHomework;
+
+public class CourseManager {
+	public void addCourse(Course course) {
+		System.out.println("Kurs eklendi : "+course.name);
+	}
+	public void update(Course course) {
+		System.out.println("Kurs Güncellendi : "+course.name);
+	}
+	public void removeCourse(Course course) {
+		System.out.println("Kurs Kaldirildi : "+course.name);
+	}
+}
