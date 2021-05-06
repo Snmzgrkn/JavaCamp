d992ea268229f71c1d7f36dc4428fc5f00133080
Gürkan Sönmez <56911478+Snmzgrkn@users.noreply.github.com>
2021-05-06 13:50:29 +0300

    Delete Day4/GameTest directory

--- Day4/GameTest/src/Abstract/GameService.java
+++
@@ -1,9 +0,0 @@
-package Abstract;
-
-import Entities.Campaign;
-import Entities.Customer;
-import Entities.Game;
-
-public interface GameService {
-	void sell(Game game,Customer customer,Campaign campign);
-}
